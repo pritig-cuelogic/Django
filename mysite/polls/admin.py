c1ee4335a70cb9d3856ac1a2debbb5afe980068e
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-02-01 19:13:05 +0530

    change

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+	    (None,               {'fields': ['question_text']}),
+	    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	list_display = ('question_text', 'pub_date')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+	inlines = [ChoiceInline]
+admin.site.register(Question, QuestionAdmin)
