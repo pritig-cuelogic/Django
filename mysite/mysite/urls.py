98c3c41876c621cee652bee7dfcea33975e3e68c
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-02-02 18:49:53 +0530

    change

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+from polls.forms import LoginForm
+
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm},name = 'login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),
 ]
