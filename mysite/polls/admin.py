43270d442a4f287e68c7d410c23d781eaf5a70f5
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-02-06 16:46:08 +0530

    Changes in following file. 1. remove space after none in mysite/polls/admin.py 2.80 char rule applied rest of files.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -8,8 +8,9 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets = [
-	    (None,               {'fields': ['question_text']}),
-	    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	    (None, {'fields': ['question_text']}),
+	    ('Date information', {'fields': ['pub_date'],
+	                         'classes': ['collapse']}),
 	]
 	list_display = ('question_text', 'pub_date')
 	list_filter = ['pub_date']
